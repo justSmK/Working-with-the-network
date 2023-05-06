e95eb613374b35ed93d32e8671193e3d98ed21d9
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-06 20:58:51 +0300

    Created request

--- Working with the network/ViewController.swift
+++ Working with the network/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makeRequest()
     }
 
 
+    private func makeRequest() {
+        var request = URLRequest(url: URL(string: "https://v2.jokeapi.dev/joke/Any")!)
+        request.allHTTPHeaderFields = ["authToken": "nil"]
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            print(String(decoding: data!, as: UTF8.self))
+            print(error)
+        }
+        
+        task.resume()
+    }
 }
 
