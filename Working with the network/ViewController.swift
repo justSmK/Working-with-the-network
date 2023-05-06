44bf8d5e2c40c774d6eadbea7e1d2cdbf4d8ad1b
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-06 20:45:15 +0300

    Init project

---
+++ Working with the network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Working with the network
+//
+//  Created by Sergei Semko on 5/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
